df357057de230ef9c85c95033fb51b44d1c1f338
prerna mahabare <prernasmahabare24@gmail.com>
2025-01-20 12:57:37 +0530

    Authentication with frontend

--- Week6/6.1/index.js
+++ Week6/6.1/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const jwt = require("jsonwebtoken");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 const secretKey = "HelloURMad";
 
