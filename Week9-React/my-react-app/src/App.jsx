1acd7ba8c20d19ec9c1677fb5e65de0625a00c1d
prerna mahabare <prernasmahabare24@gmail.com>
2025-02-21 21:10:57 +0530

    Dependency Array

--- Week9-React/my-react-app/src/App.jsx
+++ Week9-React/my-react-app/src/App.jsx
@@ -4,19 +4,34 @@ function App() {
 
   const [count, setCount] = useState(0);
 
-  function increseCount() {
-    setCount(count + 1);
-  }
+  // function increseCount() {
+  //   setCount(count + 1);
+  // }
 
-  useEffect(function () {
-    setInterval(function () {
-      setCount(count => count + 1);
+  useEffect(() =>{
+    let counter = setInterval(()=>{
+      setCount(count => count + 1)
     }, 1000)
-  }, []);
+
+    // If we dont unmount or cleanup setInterval then it switch like 1,3,5,6 bez for 1000 sec it running in background show it skips 1 no bez time is 1 sec. 
+
+    // If we use this then it will stop the counter to update when componet is unmounted(means in waiting period clock does not update).
+    return function(){
+      clearInterval(counter);
+    }
+  }, [])// Empty dependency array means this runs once when the component mounts(means starting at the application).
+
+
+  // useEffect(()=>{
+  //   setInterval(()=>{
+  //     console.log("hello from useeffect");
+  //   })
+  // },[count])
 
   return (
     <>
     <h1>{count}</h1>
+
       {/* <button onClick={increseCount}>count{count}</button> */}
 
       Hello i m prerna
