828e93cd5982b47345b75ec4d9aa4a9c7687c6ee
prerna mahabare <prernasmahabare24@gmail.com>
2025-04-09 19:17:33 +0530

    Combine atom

--- Week11/recoil_deep_drive/src/App.jsx
+++ Week11/recoil_deep_drive/src/App.jsx
@@ -1,5 +1,6 @@
 import { RecoilRoot, useRecoilValue } from 'recoil'
-import { jobAtom, networkAtom, notificationAtom, messageAtom, totalNotificationSelector } from "./Atom"
+// import { jobAtom, networkAtom, notificationAtom, messageAtom, totalNotificationSelector } from "./Atom"
+import { ItemAtom, ItemSelector } from './Atom';
 import { useMemo } from 'react';
 
 function App() {
@@ -11,18 +12,14 @@ function App() {
 }
 
 function Main() {
-  const notification = useRecoilValue(notificationAtom);
-  const message = useRecoilValue(messageAtom);
-  const jobs = useRecoilValue(jobAtom);
-  const network = useRecoilValue(networkAtom);
+  const allItem = useRecoilValue(ItemAtom);
 
-  // by using selector
-  // const totalNotificationCount = useRecoilValue(totalNotificationSelector)
+  const notification = allItem.notification;
+  const message = allItem.message;
+  const jobs = allItem.job;
+  const network = allItem.network;
 
-  // By using memo
-  const totalNotificationCount = useMemo(() => {
-    return notification + message + jobs + network
-  }, [notification, message, jobs, network])
+  const SumOfItem = useRecoilValue(ItemSelector);
 
 
   return (
@@ -32,9 +29,38 @@ function Main() {
       <button>Messages{message}</button>
       <button>Jobs{jobs}</button>
       <button>Network{network}</button>
-      <button>Me{totalNotificationCount}</button>
+      <button>Me{SumOfItem}</button>
     </>
   )
 }
+
+// Simple way By atom and Selector
+
+// function Main() {
+//   const notification = useRecoilValue(notificationAtom);
+//   const message = useRecoilValue(messageAtom);
+//   const jobs = useRecoilValue(jobAtom);
+//   const network = useRecoilValue(networkAtom);
+
+//   // by using selector
+//   // const totalNotificationCount = useRecoilValue(totalNotificationSelector)
+
+//   // By using memo
+//   const totalNotificationCount = useMemo(() => {
+//     return notification + message + jobs + network
+//   }, [notification, message, jobs, network])
+
+
+//   return (
+//     <>
+//       <button>Home</button>
+//       <button>Notification{notification}</button>
+//       <button>Messages{message}</button>
+//       <button>Jobs{jobs}</button>
+//       <button>Network{network}</button>
+//       <button>Me{totalNotificationCount}</button>
+//     </>
+//   )
+// }
 
 export default App
--- Week11/recoil_deep_drive/src/Atom.js
+++ Week11/recoil_deep_drive/src/Atom.js
@@ -1,43 +1,55 @@
 import { atom, selector } from "recoil";
 
-export const notificationAtom = atom({
-    key: "notificationAtom ",
-    default: 10
-})
+// export const notificationAtom = atom({
+//     key: "notificationAtom ",
+//     default: 10
+// })
 
-export const messageAtom = atom({
-    key: "messageAtom",
-    default: 0
-})
+// export const messageAtom = atom({
+//     key: "messageAtom",
+//     default: 0
+// })
 
-export const jobAtom = atom({
-    key: "jobAtom",
-    default: 14
-})
+// export const jobAtom = atom({
+//     key: "jobAtom",
+//     default: 14
+// })
 
-export const networkAtom = atom({
-    key: "networkAtom",
-    default: 10
-})
+// export const networkAtom = atom({
+//     key: "networkAtom",
+//     default: 10
+// })
 
-// export const ItemAtom = atom({
-//     key:"ItemAtom",
-//     default:({
-//         notification: 0,
-//         message:10,
-//         job:14,
-//         network:20
+// export const totalNotificationSelector = selector({
+//     key: "totalNotificationSelector",
+//     get: (function ({ get }) {
+//         const notification = get(notificationAtom)
+//         const message = get(messageAtom)
+//         const job = get(jobAtom)
+//         const network = get(networkAtom)
+
+//         return notification + message + job + network;
 //     })
 // })
 
-export const totalNotificationSelector = selector({
-    key: "totalNotificationSelector",
-    get: (function ({ get }) {
-        const notification = get(notificationAtom)
-        const message = get(messageAtom)
-        const job = get(jobAtom)
-        const network = get(networkAtom)
-
-        return notification + message + job + network;
+export const ItemAtom = atom({
+    key:"ItemAtom",
+    default:({
+        notification: 0,
+        message:10,
+        job:14,
+        network:20
     })
 })
+
+export const ItemSelector = selector({
+    key:"ItemSelector",
+    get:(function ({get}){
+
+        const allItem = get(ItemAtom);
+
+        return allItem.notification + allItem.message + allItem.job + allItem.network;
+    })
+})
+
+
